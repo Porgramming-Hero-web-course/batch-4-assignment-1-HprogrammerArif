c6d11d56755e93b105937b2a39a3710b10606fd8
HprogrammerArif <programmerhabluarif@gmail.com>
2024-11-11 11:19:33 +0600

    solve second problem using spread operator and js powerfull new set method

--- src/problem-2.ts
+++ src/problem-2.ts
@@ -1,8 +1,21 @@
 {
 //
+//Problem 2: Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
 
 
+const removeDuplicates = (elements: number[]):number[] => {
+  return [...new Set(elements)]
+}
 
+
+// Sample Input:
+const unique  = removeDuplicates([1, 2, 2, 3, 4, 4, 5])
+
+// Sample Output:
+console.log(unique);
+//[1, 2, 3, 4, 5]
 
   //
 }
+
+
